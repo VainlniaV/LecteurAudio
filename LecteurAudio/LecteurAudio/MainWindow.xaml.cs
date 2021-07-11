9337b4ebdd49785d351552f72645db4984416074
Vain|niaV <38719846+VainlniaV@users.noreply.github.com>
2021-07-11 11:05:34 +0200

    Add files via upload

---
+++ LecteurAudio/LecteurAudio/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace LecteurAudio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MediaPlayer player = new MediaPlayer();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Update;
+            timer.Start();
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            if(player.Source != null)
+            {
+                Progress.Minimum = 0;
+                Progress.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
+                Progress.Value = player.Position.TotalSeconds;
+            }
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if(fileDialog.ShowDialog() == true)
+            {
+                player.Open(new Uri(fileDialog.FileName));
+                player.Play();
+                FilePath.Text = fileDialog.FileName;
+            }
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void VolumeBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            player.Volume = VolumeBar.Value / 100;
+        }
+    }
+}
